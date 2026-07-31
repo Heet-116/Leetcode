dd0bf1f5ac8f7025ae258981152112394df585c7
Heet-116 <heetpatel194@gmail.com>
2026-07-31 18:40:57 +0530

    Time: 4 ms (86.04%) | Memory: 44.9 MB (48.99%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+
+        int ans=0;
+        int i=0;
+        int j=0;
+
+        while(i<g.size() && j<s.size()){
+            if(g[i]<=s[j]){
+                ans++;
+                if(i<g.size()){
+                i++;
+                }
+                if(j<s.size()){
+                j++;
+                }
+            }
+            else{
+                j++;
+            }
+        }
+        return ans;
+    }
+};
