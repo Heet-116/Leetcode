68d867b555ba4c79dbfb7f4003d7696eef9ae504
Heet-116 <heetpatel194@gmail.com>
2026-08-02 12:14:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (99.93%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
+        int i=0;
+        int j=piles.size()-1;
+        int alice=0;
+        int bob=0;
+        while(i<j){
+            alice+=max(piles[i],piles[j]);
+            bob+=min(piles[i],piles[j]);
+            i++;
+            j--;
+        }  
+        if(alice>bob)
         return true;
+        else
+        return false;
     }
 };
