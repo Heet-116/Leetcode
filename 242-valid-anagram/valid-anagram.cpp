ed7c44c0626245b3fa248f24c574ff1919a69b01
Heet-116 <heetpatel194@gmail.com>
2026-08-10 14:26:03 +0530

    Time: 4 ms (34.31%) | Memory: 9.8 MB (26.20%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        map <char,int> mpp;
+        map <char,int> mpp1;
+        for(int i=0;i<s.size();i++){
+            mpp[s[i]]++;
+            mpp1[t[i]]++;
+        }
+        return mpp==mpp1;
+    }
+};
